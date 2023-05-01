cd25b698b64fb67d7fced04b76a5cbde7784bf93
Sebiten <sebdevspace@gmail.com>
2023-05-01 19:19:17 -0300

    logo done

---
+++ src/data/services.js
@@ -0,0 +1,69 @@
+import {
+  faCode,
+  faMobileAlt,
+  faPencilAlt,
+  faCodeBranch,
+  faPalette,
+} from "@fortawesome/free-solid-svg-icons";
+export const services = [
+  {
+    icon: faCode,
+    color: "text-green-500",
+    title: "Front-End Development",
+    titleEs: "Desarrollo Front-End",
+    description:
+      "Development of modern and responsive user interfaces using the latest web technologies such as HTML5, CSS3, and JavaScript.",
+    descriptionEs:
+      "Desarrollo de interfaces de usuario modernas y responsivas utilizando las últimas tecnologías web como HTML5, CSS3 y JavaScript.",
+  },
+  {
+    icon: faMobileAlt,
+    color: "text-blue-500",
+    title: "Responsive Design",
+    titleEs: "Diseño Responsivo",
+    description:
+      "Creation of websites that automatically adapt to different screen sizes to ensure a smooth and consistent user experience on all devices.",
+    descriptionEs:
+      "Creación de sitios web que se adaptan automáticamente a diferentes tamaños de pantalla para garantizar una experiencia de usuario fluida y consistente en todos los dispositivos.",
+  },
+  {
+    icon: faPalette,
+    color: "text-yellow-500",
+    title: "UI/UX Design",
+    titleEs: "Diseño UI/UX",
+    description:
+      "Design of intuitive and visually appealing user interfaces that are easy to navigate and use.",
+    descriptionEs:
+      "Diseño de interfaces de usuario intuitivas y visualmente atractivas que son fáciles de navegar y utilizar.",
+  },
+  {
+    icon: faCodeBranch,
+    color: "text-purple-500",
+    title: "Version Control",
+    titleEs: "Control de Versiones",
+    description:
+      "Utilization of version control systems such as Git to manage code changes and collaborate effectively with other developers.",
+    descriptionEs:
+      "Utilización de sistemas de control de versiones como Git para gestionar los cambios de código y colaborar de manera efectiva con otros desarrolladores.",
+  },
+  {
+    icon: faPencilAlt,
+    color: "text-red-500",
+    title: "Wireframing",
+    titleEs: "Creación de Wireframes",
+    description:
+      "Wireframing: Creation of visual outlines and low-fidelity mockups to plan and refine the structure and layout of a website or application",
+    descriptionEs:
+      "Creación de esquemas visuales y maquetas de baja fidelidad para planificar y refinar la estructura y el diseño de un sitio web o aplicación.",
+  },
+  {
+    icon: faCode,
+    color: "text-green-500",
+    title: "Web Performance Optimization",
+    titleEs: "Optimización del rendimiento web",
+    description:
+      "Improvement of website performance through techniques such as code optimization, image compression, and caching.",
+    descriptionEs:
+      "Mejora del rendimiento de los sitios web a través de técnicas como la optimización del código, la compresión de imágenes y el almacenamiento en caché.",
+  },
+];
