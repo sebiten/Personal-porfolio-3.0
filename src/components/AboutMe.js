d33084f87d1fa69a6492f57b15581cfcdf99dcae
Sebiten <sebdevspace@gmail.com>
2023-05-08 17:00:35 -0300

    language cv

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,7 +1,9 @@
 import { motion } from "framer-motion";
 import { useTranslate } from "@/hooks/useTranslate";
+import CVDownloadButton from "./CVDowloadButton";
 const AboutMe = () => {
   const { language, setIsClicked } = useTranslate();
+
 
   return (
     <section
@@ -36,16 +38,10 @@ const AboutMe = () => {
             ? "Mi nombre es Sebastian Burgos y soy un programador de 24 años apasionado por la tecnología. Me encantan todo tipo de desafíos y siempre busco aprender nuevas habilidades y técnicas para mejorar mi trabajo. Una de mis fortalezas como programador es mi capacidad para aprender rápidamente. Me motiva la perspectiva de crecimiento personal y siempre busco formas de mejorar tanto profesional como personalmente. Creo que esta mentalidad de mejora constante es esencial para lograr el éxito en cualquier campo, y estoy emocionado de ver a dónde me llevarán mis habilidades e intereses en el futuro."
             : "My name is Sebastian Burgos and I am a 24-year-old programmer who is passionate about technology. I love all kinds of challenges and always seek to learn new skills and techniques to improve my work. One of my strengths as a programmer is my ability to learn quickly. I am motivated by the prospect of personal growth and always look for ways to improve myself, both professionally and personally. I believe that this mindset of constant improvement is essential for achieving success in any field, and I am excited to see where my skills and interests will take me in the future"}
         </p>
-        <motion.a
-          href="https://drive.google.com/file/d/11PnlPHlZfzoh2Xgb1qZdXz33N_bhrJxi/view?usp=sharing"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="bg-green-300 text-gray-800 rounded-full py-3 px-6 font-semibold text-lg shadow-md hover:bg-gray-300 "
-          whileHover={{ scale: 1.1 }}
-          transition={{ duration: 0.3 }}
-        >
-         {language === 'es' ? "Descargar CV" : "Download CV"}
-        </motion.a>
+       <CVDownloadButton
+        language={language}
+        setIsClicked={setIsClicked}
+       />
       </motion.div>
     </section>
   );
---
+++ src/components/CVDowloadButton.js
@@ -0,0 +1,39 @@
+import { motion } from 'framer-motion';
+
+const CVDownloadButton = ({ language, toggleLanguage }) => {
+  const getDownloadInfo = () => {
+    if (language === 'es') {
+      return {
+        url: 'https://drive.google.com/file/d/11n029HpALAom2oAKmaV8h7SmVQC6q1DO/view?usp=sharing',
+        text: 'Descargar CV',
+      };
+    } else {
+      return {
+        url: 'https://drive.google.com/file/d/11PnlPHlZfzoh2Xgb1qZdXz33N_bhrJxi/view?usp=sharing',
+        text: 'Download CV',
+      };
+    }
+  };
+
+  const { url, text } = getDownloadInfo();
+
+  // const handleLanguageChange = () => {
+  //   toggleLanguage();
+  // };
+
+  return (
+    <motion.a
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="bg-green-300 text-gray-800 rounded-full py-3 px-6 font-semibold text-lg shadow-md hover:bg-gray-300 "
+      whileHover={{ scale: 1.1 }}
+      transition={{ duration: 0.3 }}
+      // onClick={handleLanguageChange}
+    >
+      {text}
+    </motion.a>
+  );
+};
+
+export default CVDownloadButton;
