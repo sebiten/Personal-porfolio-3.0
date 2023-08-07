2e770bdc3dc1efd1811afced677bd06c4670b7f6
Sebiten <sebdevspace@gmail.com>
2023-08-07 13:37:55 -0300

    cv

--- src/components/CVDowloadButton.js
+++ src/components/CVDowloadButton.js
@@ -4,12 +4,12 @@ const CVDownloadButton = ({ language, toggleLanguage }) => {
   const getDownloadInfo = () => {
     if (language === "es") {
       return {
-        url: "https://drive.google.com/file/d/11n029HpALAom2oAKmaV8h7SmVQC6q1DO/view?usp=sharing",
+        url: "https://drive.google.com/file/d/1dm0LYQ70kT8R8eQ7BpoYQ_lEZEUJCNt2/view?usp=sharing",
         text: "Descargar CV",
       };
     } else {
       return {
-        url: "https://drive.google.com/file/d/11PnlPHlZfzoh2Xgb1qZdXz33N_bhrJxi/view?usp=sharing",
+        url: "https://drive.google.com/file/d/1g1qKSumwPeY95AqVfAQYEPLsJV6pyB1P/view?usp=sharing",
         text: "Download CV",
       };
     }
